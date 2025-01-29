6f7764646191f384963fe4e9612e51a70825c3e5
Alex Ho <alex.the.ho@gmail.com>
2025-01-28 23:03:20 -0500

    inittial commit
    - installed express
    - typescript and config files
    - axios for api
    - initial server code

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export interface LocationData {
+    status: string;
+    continent: string;
+    country: string;
+    countryCode: string;
+    regionName: string;
+    city: string;
+    zip: string;
+    lat: number;
+    lon: number;
+    timezone: string;
+    currency: string;
+    isp: string;
+    org: string;
+    as: string;
+    reverse: string;
+    mobile: boolean;
+    proxy: boolean;
+    ip: string;
+    cached: boolean;
+    cacheTimestamp: number;
+  }
+  
